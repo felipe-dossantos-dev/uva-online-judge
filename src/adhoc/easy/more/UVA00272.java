0f5a7c03dff08006a0853cec51d1de81d226a2ff
fgsantos89 <santos.flip@gmail.com>
2015-12-14 20:22:59 -0200

    primeiro ex feito do competitive programming

--- src/adhoc/easy/more/UVA00272.java
+++ src/adhoc/easy/more/UVA00272.java
@@ -1,26 +1,20 @@
-package uvaonlinejudge;
+package adhoc.easy.more;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Locale;
 
-public class UvaOnlineJudge {
+public class UVA00272 {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
         BufferedReader entrada = new BufferedReader(new InputStreamReader(
                 System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(
-//                new FileInputStream("/home/felipe/entradas.txt")));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-//                new FileOutputStream("/home/felipe/saida.txt")));
         String linha = entrada.readLine();
         boolean primeira = true;
         while (linha != null) {
