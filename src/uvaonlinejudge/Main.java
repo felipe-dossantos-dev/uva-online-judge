e50f3fedee07924b65f29490844cb35f2ce4b9db
felipe.santos <santos.flip@gmail.com>
2015-12-15 15:53:10 -0200

    +4, alterações nos pacotes e no nome da classe principal

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -2,6 +2,8 @@ package uvaonlinejudge;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,14 +13,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(
-                System.in));
 //        BufferedReader entrada = new BufferedReader(new InputStreamReader(
-//                new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
-        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-                System.out));
+//                System.in));
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(
+                new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
 //        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-//                new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
+//                System.out));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
         String linha = entrada.readLine();
         int qtdTeste = Integer.parseInt(linha);
         for (int i = 0; i < qtdTeste; i++) {
