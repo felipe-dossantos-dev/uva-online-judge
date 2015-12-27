33c0e8ca769906a0df2a613c85b5faeef605cf94
fgsantos89 <santos.flip@gmail.com>
2015-12-27 18:07:00 -0200

    +1 ad hoc games card

--- src/adhoc/game/card/UVA11678.java
+++ src/adhoc/game/card/UVA11678.java
@@ -1,29 +1,21 @@
-package uvaonlinejudge;
+package adhoc.game.card;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Locale;
 
-public class Main {
+public class UVA11678 {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
         BufferedReader entrada = new BufferedReader(new InputStreamReader(
                 System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(
-//                new FileInputStream("/home/felipe/entradas.txt")));
-//        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));
-//        Scanner entrada = new Scanner(System.in);
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-//                new FileOutputStream("/home/felipe/saidas.txt")));
         String linha = entrada.readLine();
         String vet[] = linha.split(" ");
         int a = Integer.parseInt(vet[0]);
