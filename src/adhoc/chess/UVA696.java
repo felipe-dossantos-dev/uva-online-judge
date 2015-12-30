196ca89c4a03cee444e9e7ca8cf1f60f9b691789
fgsantos89 <santos.flip@gmail.com>
2015-12-29 23:31:27 -0200

    +1 ad hoc chess

--- src/adhoc/chess/UVA696.java
+++ src/adhoc/chess/UVA696.java
@@ -1,28 +1,20 @@
-package uvaonlinejudge;
+package adhoc.chess;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Locale;
 
-public class Main {
+public class UVA696 {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(
-//                System.in));
         BufferedReader entrada = new BufferedReader(new InputStreamReader(
-                new FileInputStream("/home/felipe/entradas.txt")));
-//        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));
-//        Scanner entrada = new Scanner(System.in);
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-//                System.out));
+                System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("/home/felipe/saidas.txt")));
+                System.out));
         String linha = entrada.readLine();
         String vet[] = linha.split(" ");
         int rows = Integer.parseInt(vet[0]);
