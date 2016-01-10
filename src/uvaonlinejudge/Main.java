b8a5d8e33b88fb5395633c1037a8719e13b9994f
fgsantos89 <santos.flip@gmail.com>
2016-01-10 18:04:16 -0200

    comeco de um novo

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -7,35 +7,42 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("/home/felipe/entradas.txt")));
+//        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("/home/felipe/entradas.txt")));
 //        Scanner entrada = new Scanner(new FileInputStream("/home/felipe/entradas.txt"));
 //        Scanner entrada = new Scanner(new FileInputStream(args[0]));
 //        Scanner entrada = new Scanner(System.in);
-        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/felipe/saidas.txt")));
-        int qtdTestes = Integer.parseInt(entrada.readLine());
-        for (int i = 0; i < qtdTestes; i++) {
-            String linha = entrada.readLine();
-            String vet[] = linha.split("\\s+");
-            long sum = Long.parseLong(vet[0]);
-            long dif = Long.parseLong(vet[1]);
-            long z = (sum + dif) / 2;
-            long w = (sum - dif) / 2;
-            if (z + w == sum && z >= 0 && w >= 0 && dif <= sum) {
-                long min = Math.min(w, z);
-                long max = Math.max(w, z);
-                saida.write(max + " " + min + "\n");
-            } else {
-                saida.write("impossible\n");
-            }
+//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/felipe/saidas.txt")));
+        String linha = entrada.readLine();
+        char pag[][] = new char[60][60];
+        for (int i = 0; i < 60; i++) {
+            Arrays.fill(pag[i], '.');
         }
+        while (!linha.equals(".EOP")) {
+            
+            
+            linha = entrada.readLine();
+        }
+        //printar pagina
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 60; j++) {
+                saida.write(pag[i][j]);
+            }
+            saida.write("\n");
+        }
+        saida.write("\n");
+        for (int i = 0; i < 60; i++) {
+            saida.write('.');
+        }
+        saida.write("\n");
         saida.flush();
     }
 }
