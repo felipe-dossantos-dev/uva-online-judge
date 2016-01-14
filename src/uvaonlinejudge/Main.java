7ff292ea83ecdf2264a32d37dcb5f9c23d622dcf
felipe.santos <santos.flip@gmail.com>
2016-01-14 15:58:52 -0200

    começo de estudo de Data Structure

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -7,124 +7,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.GregorianCalendar;
+import java.util.BitSet;
 import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("/home/felipe/entradas.txt")));
+//        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
 //        Scanner entrada = new Scanner(new FileInputStream("/home/felipe/entradas.txt"));
 //        Scanner entrada = new Scanner(new FileInputStream(args[0]));
 //        Scanner entrada = new Scanner(System.in);
-        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/felipe/saidas.txt")));
+//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
         String linha = entrada.readLine();
-        int qtdTestes = Integer.parseInt(linha);
-        for (int i = 1; i <= qtdTestes; i++) {
-            linha = entrada.readLine();
-            int dia = Integer.parseInt(linha.substring(2, 4));
-            int mes = Integer.parseInt(linha.substring(0, 2)) - 1;
-            int ano = Integer.parseInt(linha.substring(4, 8));
-            GregorianCalendar gc = new GregorianCalendar(ano, mes, dia);
-            gc.add(GregorianCalendar.WEEK_OF_YEAR, 40);
-            String sign = "";
-            int day = gc.get(GregorianCalendar.DATE);
-            int month = gc.get(GregorianCalendar.MONTH);
-            switch (month) {
-                case GregorianCalendar.JANUARY:
-                    if (day < 21) {
-                        sign = "capricorn";
-                    } else {
-                        sign = "aquarius";
-                    }
-                    break;
-                case GregorianCalendar.FEBRUARY:
-                    if (day < 20) {
-                        sign = "aquarius";
-                    } else {
-                        sign = "pisces";
-                    }
-                    break;
-                case GregorianCalendar.MARCH:
-                    if (day < 21) {
-                        sign = "pisces";
-                    } else {
-                        sign = "aries";
-                    }
-                    break;
-                case GregorianCalendar.APRIL:
-                    if (day < 21) {
-                        sign = "aries";
-                    } else {
-                        sign = "taurus";
-                    }
-                    break;
-                case GregorianCalendar.MAY:
-                    if (day < 22) {
-                        sign = "taurus";
-                    } else {
-                        sign = "gemini";
-                    }
-                    break;
-                case GregorianCalendar.JUNE:
-                    if (day < 22) {
-                        sign = "gemini";
-                    } else {
-                        sign = "cancer";
-                    }
-                    break;
-                case GregorianCalendar.JULY:
-                    if (day < 23) {
-                        sign = "cancer";
-                    } else {
-                        sign = "leo";
-                    }
-                    break;
-                case GregorianCalendar.AUGUST:
-                    if (day < 22) {
-                        sign = "leo";
-                    } else {
-                        sign = "virgo";
-                    }
-                    break;
-                case GregorianCalendar.SEPTEMBER:
-                    if (day < 24) {
-                        sign = "virgo";
-                    } else {
-                        sign = "libra";
-                    }
-                    break;
-                case GregorianCalendar.OCTOBER:
-                    if (day < 24) {
-                        sign = "libra";
-                    } else {
-                        sign = "scorpio";
-                    }
-                    break;
-                case GregorianCalendar.NOVEMBER:
-                    if (day < 23) {
-                        sign = "scorpio";
-                    } else {
-                        sign = "sagittarius";
-                    }
-                    break;
-                case GregorianCalendar.DECEMBER:
-                    if (day < 23) {
-                        sign = "sagittarius";
-                    } else {
-                        sign = "capricorn";
-                    }
-                    break;
-                default:
-                    sign = "";
-            }
-            saida.write(i + " " + String.format("%02d/%02d/%d ", gc.get(GregorianCalendar.MONTH) + 1,
-                    gc.get(GregorianCalendar.DATE),
-                    gc.get(GregorianCalendar.YEAR)) + sign + "\n");
-        }
+        int a = 320;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(-a));
+        a = a & ~(a & (-a));
+        System.out.println(Integer.toBinaryString(a));
         saida.flush();
     }
 }
