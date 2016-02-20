c4bb8b8a54d45708228c4cf08bec1d7316b4df9c
fgsantos89 <santos.flip@gmail.com>
2016-02-20 18:26:40 -0200

    Merge branch 'master' of https://github.com/fgsantos89/uva-online-judge
    # Conflicts:
    #	src/uvaonlinejudge/Main.java

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -1,26 +1,25 @@
 package uvaonlinejudge;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
-
+    
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
 //        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("/home/felipe/entradas.txt")));
-//        Scanner entrada = new Scanner(new FileInputStream("/home/felipe/entradas.txt"));
+//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("/home/felipe/entradas.txt")));
+        Scanner entrada = new Scanner(new FileInputStream("/home/felipe/entradas.txt"));
 //        Scanner entrada = new Scanner(System.in);
 //        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/felipe/saidas.txt")));
-        String linha = entrada.readLine();
-        int n = Integer.parseInt(linha);
+        int n = entrada.nextInt();
         
         saida.flush();
     }
