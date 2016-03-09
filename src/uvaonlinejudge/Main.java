273e22c4b57bec19eaddfee6f39b575b38359aca
felipe.santos <santos.flip@gmail.com>
2016-03-09 15:28:59 -0300

    começo dos recursivos

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -7,23 +7,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 public class Main {
 
+    private static int surplus;
+    private static int deficit;
+    private static int max;
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
+//        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
 //        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));//C:\Users\felipe.santos\Documents\
 //        Scanner entrada = new Scanner(System.in);
-        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
+//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
         String linha = entrada.readLine();
-       
+        int testes = Integer.parseInt(linha);
+        linha = entrada.readLine();
+        while (!linha.contains("0000")) {
+            String vet[] = linha.split("\\s+");
+            
+            
+            linha = entrada.readLine();
+        }
         saida.flush();
     }
 }
