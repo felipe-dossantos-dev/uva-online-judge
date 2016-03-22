89a04f1bc48340ba620e92788a8fcd8d7732aa90
felipe.santos <santos.flip@gmail.com>
2016-03-22 15:58:36 -0300

    começo dos recursivos

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -1,37 +1,84 @@
 package uvaonlinejudge;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
-    private static int surplus;
-    private static int deficit;
-    private static int max;
+    public static List<Call> lista;
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
 //        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
-//        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));//C:\Users\felipe.santos\Documents\
+//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
+        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));//C:\Users\felipe.santos\Documents\
 //        Scanner entrada = new Scanner(System.in);
 //        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
-        String linha = entrada.readLine();
-        int testes = Integer.parseInt(linha);
-        linha = entrada.readLine();
-        while (!linha.contains("0000")) {
-            String vet[] = linha.split("\\s+");
-            
-            
-            linha = entrada.readLine();
+        int testes = entrada.nextInt();
+        int tmp = 1;
+        saida.write("CALL FORWARDING OUTPUT\n");
+        for (int i = 0; i < testes; i++) {
+            if (tmp != 0) {
+                leituraSistema(entrada);
+            } else {
+                tmp = entrada.nextInt();
+                saida.write("SYSTEM " + (i + 1) + "\n");
+                while (entrada.hasNext() && tmp != 9000) {
+                    int time = tmp;
+                    int receiver = entrada.nextInt();
+                    int rings = backtrack(time, receiver, receiver, 0);
+                    saida.write(String.format("AT %04d CALL TO %04d RINGS %04d\n", time, receiver, rings));
+                    tmp = entrada.nextInt();
+                }
+            }
         }
+        saida.write("END OF OUTPUT\n");
         saida.flush();
+    }
+
+    public static void leituraSistema(Scanner entrada) {
+        lista = new ArrayList<>();
+        int tmp = entrada.nextInt();;
+        while (tmp != 0) {
+            Call c = new Call();
+            c.receiver = tmp;
+            c.begin = entrada.nextInt();
+            c.duration = entrada.nextInt();
+            c.pass = entrada.nextInt();
+            lista.add(c);
+            tmp = entrada.nextInt();
+        }
+    }
+
+    public static int backtrack(int time, int receiver, int initial, int calls) {
+        if (receiver == initial && calls > 0) {
+            return 9999;
+        } else {
+            for (int i = 0; i < lista.size(); i++) {
+                Call c = lista.get(i);
+                if (c.receiver == receiver
+                        && time >= c.begin
+                        && time <= c.begin + c.duration) {
+                    return backtrack(time, c.pass, initial, ++calls);
+                }
+            }
+            return receiver;
+        }
+    }
+
+    public static class Call {
+
+        public int receiver;
+        public int pass;
+        public int begin;
+        public int duration;
     }
 }
