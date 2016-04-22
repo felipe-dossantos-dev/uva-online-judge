45d6d0fd7dae2fc046786275bda6834e8cf70a78
felipe.santos <santos.flip@gmail.com>
2016-04-22 14:49:29 -0300

    +1 recursive medium

--- src/uvaonlinejudge/Main.java
+++ src/uvaonlinejudge/Main.java
@@ -1,73 +1,82 @@
 package uvaonlinejudge;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 
-    public static Set<List<Integer>> respostas = new LinkedHashSet<>();
-    public static int soma;
-    public static List<Integer> numeros = new ArrayList<>();
-    public static boolean vis[];
+    public static int vis[] = new int[300];
+    public static boolean graph[][] = new boolean[300][300];
+    public static boolean temCiclo;
+    public static int n;
+    public static List<Integer> caminho;
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
 //        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt")));
 //        Scanner entrada = new Scanner(new FileInputStream("C:\\Users\\felipe.santos\\Documents\\entradas.txt"));///home/felipe/
-        Scanner entrada = new Scanner(System.in);
-        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
-//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
-        soma = entrada.nextInt();
-        int qtd = entrada.nextInt();
-        while (soma != 0 && qtd != 0) {
-            respostas.clear();
-            numeros.clear();
-            vis = new boolean[qtd];
-            for (int i = 0; i < qtd; i++) {
-                numeros.add(entrada.nextInt());
+//        Scanner entrada = new Scanner(System.in);
+//        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\felipe.santos\\Documents\\saidas.txt")));
+        String linha = entrada.readLine();
+        while (linha != null) {
+            n = Integer.parseInt(linha);
+            Arrays.fill(vis, 0);
+            for (int i = 0; i < 300; i++) {
+                Arrays.fill(graph[i], false);
             }
-            backtrack(0, 0, new ArrayList<>());
-            saida.write("Sums of " + soma + ":\n");
-            if (respostas.size() > 0) {
-                for (List<Integer> resposta : respostas) {
-                    for (int i = 0; i < resposta.size(); i++) {
-                        saida.append(resposta.get(i) + "");
-                        if (i + 1 != resposta.size()) {
-                            saida.write("+");
-                        }
-                    }
-                    saida.write("\n");
-                }
+            temCiclo = false;
+            caminho = null;
+            linha = entrada.readLine();
+            while (!linha.contains("%")) {
+                String vet[] = linha.split("\\s+");
+                int a = Integer.parseInt(vet[0]) - 1;
+                int b = Integer.parseInt(vet[1]) - 1;
+                graph[a][b] = true;
+                linha = entrada.readLine();
+            }
+            for (int i = 0; i < n; i++) {
+                List<Integer> l = new ArrayList<>();
+                l.add(i);
+                vis[i] = 1;
+                backtrack(i, l, -1);
+                vis[i] = 0;
+            }
+            if (caminho == null) {
+                saida.write("N\n");
             } else {
-                saida.write("NONE\n");
+                for (Integer c : caminho) {
+                    saida.write(c + " ");
+                }
+                saida.write("\n");
             }
-            soma = entrada.nextInt();
-            qtd = entrada.nextInt();
+            linha = entrada.readLine();
         }
         saida.flush();
     }
 
-    private static void backtrack(int pos, int sum, List<Integer> caminho) {
-        if (sum == soma) {
-            respostas.add(caminho);
-        } else if (sum < soma) {
-            for (int i = pos; i < numeros.size(); i++) {
-                if (!vis[i]) {
-                    vis[i] = true;
-                    List<Integer> l = new ArrayList<>(caminho);
-                    l.add(numeros.get(i));
-                    backtrack(i, sum + numeros.get(i), l);
-                    vis[i] = false;
+    public static void backtrack(int posInicial, List<Integer> l, int posAtual) {
+        if (posInicial == posAtual) {
+            temCiclo = true;
+            caminho = l;
+        } else if (!temCiclo) {
+            for (int i = 0; i < n; i++) {
+                if (vis[i] == 0) {
+                    vis[i] = 1;
+                    List<Integer> l1 = new ArrayList<>(l);
+                    l.add(i);
+                    backtrack(posInicial, l1, i);
+                    vis[i] = 0;
                 }
             }
         }
